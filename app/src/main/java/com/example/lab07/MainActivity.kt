6b3f9a80a294dd0bf11b0f0dfbdaeaab4ad803bd
Cesar Samanez J <csamanezj@unsa.edu.pe>
2022-06-26 23:12:04 -0500

    Sensor Magnetómetro

--- app/src/main/java/com/example/lab07/MainActivity.kt
+++ app/src/main/java/com/example/lab07/MainActivity.kt
@@ -4,60 +4,99 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var sensorManger: SensorManager
-    private lateinit var texto: TextView
-    private lateinit var mensaje: TextView
+    private lateinit var sensorManagerAcelerometro: SensorManager
+    private lateinit var sensorManagerMagnetometro: SensorManager
+    private lateinit var textoAcelerometro: TextView
+    private lateinit var mensajeAcelerometro: TextView
+    private lateinit var textoMagnetometro: TextView
+    private lateinit var mensajeMagnetometro: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        texto=findViewById(R.id.coordenadas)
-        mensaje=findViewById(R.id.mensaje)
-
+        textoAcelerometro = findViewById(R.id.coordenadasAcelerometro)
+        mensajeAcelerometro = findViewById(R.id.mensajeAcelerometro)
+        textoMagnetometro = findViewById(R.id.coordenadasMagnetometro)
+        mensajeMagnetometro = findViewById(R.id.mensajeMagnetometro)
+        
         setUpSensorStuff()
     }
 
-    private fun setUpSensorStuff(){
-        sensorManger=getSystemService(SENSOR_SERVICE) as SensorManager
-        sensorManger.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also{
-            sensorManger.registerListener(this,it,SensorManager.SENSOR_DELAY_FASTEST,SensorManager.SENSOR_DELAY_FASTEST)
-
+    private fun setUpSensorStuff() {
+        sensorManagerAcelerometro = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManagerAcelerometro.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManagerAcelerometro.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
         }
+
+        sensorManagerMagnetometro = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManagerMagnetometro.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
+            sensorManagerMagnetometro.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_GAME
+            )
+        }
+
+        if (null == sensorManagerMagnetometro)
+            finish();
+
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type==Sensor.TYPE_ACCELEROMETER){
-            val x=event.values[0]
-            val y=event.values[1]
-            val z=event.values[2]
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
 
-            texto.text="x=${x.toInt()}\ny=${y.toInt()}\nz=${z.toInt()}"
-            if(x.toInt()==9){
-                mensaje.text="Horizontal 2"
+            textoAcelerometro.text =
+                "(ACELERÓMETRO)\nX = ${x.toInt()}\nY = ${y.toInt()}\nZ = ${z.toInt()}"
+            if (x.toInt() == 9) {
+                mensajeAcelerometro.text = "Horizontal 2"
             }
-            if(x.toInt()==-9){
-                mensaje.text="Horizontal 1"
+            if (x.toInt() == -9) {
+                mensajeAcelerometro.text = "Horizontal 1"
             }
-            if(y.toInt()==9){
-                mensaje.text="Vertical 1"
+            if (y.toInt() == 9) {
+                mensajeAcelerometro.text = "Vertical 1"
             }
-            if(y.toInt()==-9){
-                mensaje.text="Vertical 2"
+            if (y.toInt() == -9) {
+                mensajeAcelerometro.text = "Vertical 2"
             }
-            if(x.toInt() in 1..8 || x.toInt() in -1 downTo -8 || y.toInt() in 1..8 || y.toInt() in -1 downTo -8){
-                mensaje.text="Girando"
+            if (x.toInt() in 1..8 || x.toInt() in -1 downTo -8 || y.toInt() in 1..8 || y.toInt() in -1 downTo -8) {
+                mensajeAcelerometro.text = "Girando"
             }
         }
 
+        if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            val magX = event.values[0]
+            val magY = event.values[1]
+            val magZ = event.values[2]
+
+            val df = DecimalFormat("#.###")
+            df.roundingMode = RoundingMode.DOWN
+            val magnitude =
+                df.format(Math.sqrt((magX * magX) + (magY * magY) + (magZ * magZ).toDouble()))
+
+            textoMagnetometro.text =
+                "(MAGNETÓMETRO)\nX = ${magX}\nY = ${magY}\nZ = ${magZ}\nValor = ${magnitude}"
+        }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
